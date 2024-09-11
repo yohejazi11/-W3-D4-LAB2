e8308397ddf459d9535304582ad1b0a42af64c3f
yohejazi11 <yohejazi11@gmail.com>
2024-09-11 14:05:55 +0300

    add

---
+++ main.js
@@ -0,0 +1,19 @@
+
+
+setInterval(function () {
+    const Box=document.getElementById('box');
+
+    let color=Math.floor(Math.random()*16777215).toString(16);
+    let positionX=Math.random(-300,600);
+    let positionY=Math.random(-600,700);
+
+    let scaleX=Math.random(300,600);
+    let scaleY=Math.random(600,700);
+    Box.style.left=positionX+"px";
+    Box.style.top=positionY+"px";
+    Box.style.width=scaleX+"px";
+    Box.style.height=scaleY+"px";
+
+    Box.style.backgroundColor="#"+color;
+}, 2000);
+
