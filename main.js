983cc8348ec7958a649e39b1d4636ba1e679c01e
yohejazi11 <yohejazi11@gmail.com>
2024-09-11 14:15:46 +0300

    edite

--- main.js
+++ main.js
@@ -4,11 +4,11 @@ setInterval(function () {
     const Box=document.getElementById('box');
 
     let color=Math.floor(Math.random()*16777215).toString(16);
-    let positionX=Math.random(300,1000);
-    let positionY=Math.random(600,700);
+    let positionX=Math.random()*1000;
+    let positionY=Math.random()*100;
 
-    let scaleX=Math.random(300,600);
-    let scaleY=Math.random(600,700);
+    let scaleX=Math.random()*1000;
+    let scaleY=Math.random()*1000;
     Box.style.left=positionX+"px";
     Box.style.top=positionY+"px";
     Box.style.width=scaleX+"px";
